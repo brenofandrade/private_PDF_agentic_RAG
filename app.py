271967564c7337005f01e53ec753be81769589cc
Breno Andrade <andradefbreno@gmail.com>
2025-07-07 16:00:04 -0300

    update app

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ import streamlit as st
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OllamaEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.agents import initialize_agent, Tool, AgentType
 from langchain.llms import Ollama
 
 # Path where chroma will store the vectors
 PERSIST_DIR = "db"
-
-st.title("PDF Agentic RAG")
+st.set_page_config(page_title="chatPDF", page_icon="🩺", layout="wide")
+st.title("Assistente de documentos - Unimed Blumenau")
 
 # Session state for the agent
 if "agent" not in st.session_state:
     st.session_state.agent = None
 
-uploaded_files = st.file_uploader("Upload PDF files", type=["pdf"], accept_multiple_files=True)
+
+uploaded_files = st.sidebar.file_uploader("Envie seus PDFs", accept_multiple_files=True, type=['pdf'])
 
 if uploaded_files and st.button("Process Documents"):
     docs = []
